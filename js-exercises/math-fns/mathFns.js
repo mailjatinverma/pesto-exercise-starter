a43385d1a36de31cc35ff6222334c957377ece09
Jatin1983 <jatin@headrun.com>
2020-03-04 19:59:00 +0530

    day2 exercises. commit1

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,14 @@
+
+// Don't change the export values.
+// You can use the function in the Math module
+
+
+const sqrt = (number) => Math.sqrt(number);
+const power = (base, exponent) => base ** exponent;
+const round = (decimalNumber) => Math.round(decimalNumber);
+
+export {
+  sqrt,
+  power,
+  round,
+};
